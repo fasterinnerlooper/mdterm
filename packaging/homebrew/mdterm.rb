6f18252691a500dbae4c7b9db27ec77e0cc3a153
Shafiq Jetha <shafiq.jetha@outlook.com>
2026-04-03 12:34:49 -0600

    fix: update release workflow to include source hash and modify Homebrew formula for dynamic versioning

--- packaging/homebrew/mdterm.rb
+++ packaging/homebrew/mdterm.rb
@@ -1,27 +1,30 @@
 class Mdterm < Formula
   desc "Render Markdown files beautifully in the terminal"
   homepage "https://github.com/fasterinnerlooper/mdterm"
+  sha256 "SHA256_PLACEHOLDER"
   license "MIT"
+  url "https://github.com/fasterinnerlooper/mdterm/archive/refs/tags/vVERSION_PLACEHOLDER.tar.gz"
 
-  on_macos do
-    on_arm do
-      url "https://github.com/fasterinnerlooper/mdterm/releases/download/v1.1.5/mdterm-osx-arm64.tar.gz"
-      sha256 "7a027d9c0cee0f2aa766309f9dd891381cae72fa249c70938614fc243e2f09a2"
-    end
-
-    on_intel do
-      url "https://github.com/fasterinnerlooper/mdterm/releases/download/v1.1.5/mdterm-osx-x64.tar.gz"
-      sha256 "3e576577441fd87f814b34ae39bb2f1ecc41cc594af72253f1b616de532d616c"
-    end
-  end
-
-  on_linux do
-    url "https://github.com/fasterinnerlooper/mdterm/releases/download/v1.1.5/mdterm-linux-x64.tar.gz"
-    sha256 "fe44d0a29d8dba38c134093b3f517ee362573c1d9326ec6a463c7d43537db8fd"
-  end
+  depends_on "dotnet@9" => :build
 
   def install
-    bin.install "mdterm"
+    runtime_id = if OS.mac? && Hardware::CPU.arm?
+      "osx-arm64"
+    elsif OS.mac?
+      "osx-x64"
+    elsif Hardware::CPU.arm?
+      "linux-arm64"
+    else
+      "linux-x64"
+    end
+
+    system "dotnet", "publish", "MdTerm.csproj",
+      "-c", "Release",
+      "--self-contained", "false",
+      "-r", runtime_id,
+      "-o", "./bin/install"
+
+    bin.install "bin/install/mdterm"
   end
 
   test do
